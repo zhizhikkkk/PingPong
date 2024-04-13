bd93da71999d28c01759b81284e52530aa8d4375
zhizhikkkk <zhanelmamyt@gmail.com>
2024-04-13 16:32:31 +0600

    UI | mini refactoring

--- PingPong/Assets/Scripts/Ball.cs
+++ PingPong/Assets/Scripts/Ball.cs
@@ -14,8 +14,9 @@ public class Ball : MonoBehaviour
     private void Start()
     {
         ResetPosition();
+        AddStartingForce();
     }
-    private void AddStartingForce() 
+    public void AddStartingForce() 
     { 
         float x = Random.value<0.5f ? -1.0f : 1.0f;
         float y = Random.value<0.5f ? Random.Range(-1.0f,-0.5f) : Random.Range(0.5f, 1f);
@@ -34,6 +35,5 @@ public class Ball : MonoBehaviour
         _rigidbody.position = Vector3.zero;
         _rigidbody.velocity = Vector3.zero;
 
-        AddStartingForce() ;
     }
 }
--- PingPong/Assets/Scripts/GameManager.cs
+++ PingPong/Assets/Scripts/GameManager.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Ball ball;
     private int _playerScore;
     private int _computerScore;
+    public Text playerScoreText;
+    public Text computerScoreText;
+    public Puddle playerPuddle;
+    public Puddle computerPuddle;
 
     public void PlayerScores()
     {
         _playerScore++;
-        ball.ResetPosition();
+        
+        playerScoreText.text = _playerScore.ToString();
+        ResetRound();
     }
     public void ComputerScores()
     {
         _computerScore++;
-        ball.ResetPosition();
+        computerScoreText.text = _computerScore.ToString();
+        ResetRound();
     }
 
+    private void ResetRound()
+    {
+        ball.ResetPosition();
+        ball.AddStartingForce();
+        playerPuddle.ResetPosition();
+        computerPuddle.ResetPosition();
+    }
 
 }
